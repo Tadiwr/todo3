9ff1d3c3a42e62a64c1fcabacb2a6d1dc3b53a73
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-04-09 12:56:28 +0200

    removed a space lol

--- src/main/java/com/shangwa/todo3/todo/TodoService.java
+++ src/main/java/com/shangwa/todo3/todo/TodoService.java
@@ -14,11 +14,7 @@ public class TodoService {
     private TodoRepostitory todoRepo;
 
     public List<Todo> allTodos() {
-        // Todo td = new Todo("Brush your teeth");
-        // return List.of(td);
-
         return (List<Todo>) todoRepo.findAll();
     }
-
 
 }
