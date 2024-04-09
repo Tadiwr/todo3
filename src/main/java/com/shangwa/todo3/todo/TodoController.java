e983285309a0c60006a820928003f4d6cb0bfd54
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-04-09 11:51:48 +0200

    adds templates

--- src/main/java/com/shangwa/todo3/todo/TodoController.java
+++ src/main/java/com/shangwa/todo3/todo/TodoController.java
@@ -1,17 +1,11 @@
 package com.shangwa.todo3.todo;
 
-import java.util.List;
+import org.springframework.stereotype.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 public class TodoController {
 
-    @GetMapping("/todos")
-    public List<Todo> allStudents() {
-        Todo std = new Todo(1, "Learn Spring Boot");
-        return List.of(std);
-    }
+    
+
  
 }
